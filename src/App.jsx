a45710d19df890773cd55b8319659e82516f70cd
NareshInnotratLabs <nareshbabu@innotrat.in>
2024-09-18 11:45:53 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -97,7 +97,7 @@ function App() {
 // dataCheck()
 //  },[])
 
-const [access, setAccess] = useState(null);
+const [access, setAccess] = useState("");
 
 useEffect(() => {
   // Mock function to simulate fetching access (could be from localStorage, API, etc.)
@@ -243,65 +243,7 @@ useEffect(() => {
       </> 
       
       }
-        {/* <Route path="homepage" element={<Home/>} />
-        <Route path="dashboard" element={<Dashboard/>} />
-        <Route path="customer" element={<Customers/>} />
-        <Route path="supplier" element={<Suppliers/>} />
-        <Route path="logistic" element={<Logistic/>} />
-
-        <Route path="myprofile" element={<MyProfile/>} />
-        <Route path="workinprogress" element={<WorkProgress/>} />
-
-        <Route path="useractivatedeactivate" element={<UserActivateDeactivate/>} />
-        <Route path="adduser" element={<AddUser/>} />
-        <Route path="assignrepresentative" element={<AssignRepresentative/>} />
-        <Route path="filterAssignRepresentative" element={<FilterAssignRepresentative/>} />
-        <Route path="itemmaster" element={<ItemMaster/>} />
-        <Route path="categorywithpoints" element={<CategoryWithPoints/>} />
-        <Route path="segmentmaster" element={<SegmentMaster/>} />
-        <Route path="credittermmaster" element={<CreditTermMaster/>} />
-        <Route path="addretailerstock" element={<AddRetailerStock/>} />
-        <Route path="paymentapproval" element={<PaymentApproval/>} />
-        <Route path="giftitemmaster" element={<GiftItemMaster/>} />
-        <Route path="redeemgiftapproval" element={<RedeemGiftApproval/>} />
-        <Route path="allDSRdaykeyroute" element={<AllDsrDayKeyRoute/>} />
-        <Route path="retailer-summary" element={<RetailerSummary/>} />
-        <Route path="itemrequest" element={<ItemRequest/>} />
-        <Route path="visiting" element={<Visiting/>} />
-        <Route path="paymententry" element={<PaymentEntry/>} />
-        <Route path="billingtomechanic" element={<BillingToMechanic/>} />
-        <Route path="dsrKeyRouteOnDay" element={<DsrKeyRouteOnDay />} />
-        <Route path="viewQrCode" element={<ViewQrCode />} /> */}
-
         
-        {/* <Route path="representativelist" element={<RepresentativeList/>} />
-        <Route path="updateRepresentativeDetails" element={<UpdateRepresentativeDetails/>} />
-        <Route path="retailerslist" element={<RetailerList/>} />
-        <Route path="updateRetailerDetails" element={<UpdateRetailerDetails/>} />
-        <Route path="mechaniclist" element={<MechanicList/>} />
-        <Route path="updateMechanicDetails" element={<UpdateMechanicDetails/>} />
-        <Route path="DSRwiseretailersoutstandingreport" element={<DSRwiseRetailersOutstandingReport/>} />
-        <Route path="salesreport" element={<SalesReport/>} />
-        <Route path="receiptreport" element={<ReceiptReport/>} />
-        <Route path="linesperdealerreport" element={<LinesperDealerReport/>} />
-        <Route path="retailersaccountstatement" element={<RetailerAccountStatement/>} />
-        <Route path="representativevisitinghistory" element={<RepresentativeVisitingHistory/>} />
-        <Route path="DSRdayreport" element={<DSRDayReport/>} />
-        <Route path="DSRdaywisecollectionreport" element={<DSRDaywiseCollectionReport/>} />
-        <Route path="DSRdaywisesalesreport" element={<DSRDaywiseSalesReport/>} />
-        <Route path="retailerrequestreport" element={<RetailerRequestReport/>} />
-        <Route path="itemwiseretailerrequestreport" element={<ItemwiseRetailerRequestReport/>} />
-        <Route path="invoicehistory" element={<InvoiceHistory/>} />
-        <Route path="paymenthistory" element={<PaymentHistory/>} />
-        <Route path="mechanicsalesreport" element={<SalesReportMechanicwise/>} />
-        <Route path="mechanicttemwisesalesreport" element={<SalesReportMechanicwiseItemwise/>} />
-        <Route path="loyaltymechanicsalesreport" element={<LoyaltyMechanicSalesReport/>} />
-        <Route path="mechanicloyaltyreport" element={<MechanicLoyalPointsHistory/>} />
-        <Route path="invoicehistory" element={<InvoiceHistory/>} />
-        <Route path="invoicedetails/:invoiceId" element={<InvoiceDetails/>} />
-        
-        <Route path="changePassword" element={<ChangePassword/>} /> */}
-
       </Route>
     </Routes>
 
