38da4de0d94530c9f3eea1f7854d92f21d819470
lubeproject <lubeproject95@gmail.com>
2024-10-15 23:03:10 +0530

    Update ForgotPassword.js

--- src/components/authUser/ForgotPassword.js
+++ src/components/authUser/ForgotPassword.js
@@ -2,7 +2,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import "./forgotPassword.css"
 import { Link } from 'react-router-dom';
 
